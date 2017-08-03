bceaa6f943a262f994e535cadf64a1c01985fbbe
Isaaac Ruiz <isaac.ruiz02@utrgv.edu>
2017-08-02 20:46:40 -0500

    Some changes

--- C_Socks.cpp
+++ C_Socks.cpp
@@ -1,34 +1,22 @@
-//#include<iostream>
-//using namespace std;
-//
-//int main()
-//{
-//	int n; //number of socks
-//	int m; //number of days
-//	int k; //number of available colors
-//	int* c; //color of sock at index
-//	int* pairs[2];
-//
-//	cin >> n >> m >> k;
-//
-//	c = new int[n];
-//	pairs[0] = new int[m];
-//	pairs[1] = new int[m];
-//
-//	for (int i = 0; i < n; i++)
-//		cin >> c[i];
-//
-//	for (int i = 0; i < m; i++)
-//		cin >> pairs[0][i] >> pairs[1][i];
-//
-//}
-
-#include <iostream>
-#include <string>
-
+#include<iostream>
 using namespace std;
+
 int main()
 {
-	string A = "this";
-	cout << A[2];
+	const int SIZE = 200000;
+	int n; //number of socks
+	int m; //number of days
+	int k; //number of available colors
+	int c [SIZE]; //color of sock at index
+	int pairs [2][SIZE];
+
+/* 	cin >> n >> m >> k;
+
+	for (int i = 0; i < n; i++)
+		cin >> c[i];
+
+	for (int i = 0; i < m; i++)
+		cin >> pairs[0][i] >> pairs[1][i]; */
+
+	return 0;
 }
