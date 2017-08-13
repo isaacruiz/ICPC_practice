b9655b2c6dbfecdc58f559680bc370d803981ced
Isaaac Ruiz <isaac.ruiz02@utrgv.edu>
2017-08-13 16:37:42 -0500

    Finished B

--- 2017-08-09/B_Text_Document_Analysis.cpp
+++ 2017-08-09/B_Text_Document_Analysis.cpp
@@ -16,22 +16,30 @@ int main()
 
 	for (int i = 0; i < sentenceLength; ++i)
 	{
-		
 		if (sentence[i] == '(')
 		{
 			parenFlag = true;
+			charCount = 0;
 		}
 
-		if (sentence[i] = ')')
-		{
+		if (parenFlag)
+			if (sentence[i] == '_' || sentence[i] == ')')
+				if (sentence[i - 1] != '_' && sentence[i - 1] != '(')
+					parenWordCount++;
+
+		if (sentence[i] == ')')
 			parenFlag = false;
-		}
 
 		if (!parenFlag)
 		{
-
+			if (sentence[i] == '_' || sentence[i] == ')')
+				charCount = 0;
+			else
+				charCount++;
+			
+			if (charCount > maxWordLength)
+				maxWordLength = charCount;
 		}
-
 	}
 
 	cout << maxWordLength << " " << parenWordCount << endl;
